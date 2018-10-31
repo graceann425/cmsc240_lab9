040bed5ee96389c0cd5cca456961d47725528be6
Abby Lord <abby@Abbys-Air.richmond.edu>
2018-10-31 10:04:43 -0400

    updated LinkedList

--- LinkedList.h
+++ LinkedList.h
@@ -4,6 +4,8 @@
 #include <list>
 #include <vector>
 
+using namespace std;
+
 template <class T>
 class LinkedList
 {
@@ -25,5 +27,93 @@ class LinkedList
 
      LinkedList<T>& operator+=( const T& item );
 };
+
+template <class T>
+LinkedList<T>::LinkedList(){}
+
+
+template <class T>
+LinkedList<T>::LinkedList(const LinkedList<T>& other) {
+    this->theList = other.theList;
+}
+
+
+
+template <class T>
+LinkedList<T>::~LinkedList(){}
+
+
+
+template <class T>
+int LinkedList<T>::size() const{
+
+    return theList.size();
+}
+
+
+template <class T>
+void LinkedList<T>::add(T element){
+    theList.push_back(element);
+    return;
+}
+
+
+
+template <class T>
+T LinkedList<T>::get(int index) const{
+    if (index >= theList.size() || index < 0){
+        string msg = "Invalid index to get(): " + index + ", list size= " +  theList.size();
+        throw invalid_argument(msg.c_str());
+    }
+
+    list<T>::const_iterator it = theList.begin();
+    for(int i = 0; i < index; i++){
+        it++;
+    }
+
+    return (*it);
+
+}
+
+
+template <class T>
+T LinkedList<T>::remove(int index){
+    if (index >= theList.size() || index < 0){
+        string msg = "Invalid index to remove(): " + index + ", list size= " +  theList.size();
+        throw invalid_argument(msg.c_str());
+    }
+
+    list<T>::const_iterator it = theList.begin();
+    for(int i = 0; i < index; i++){
+        it++;
+    }
+    theList.remove(*it);
+        
+    return (*it);
+}
+
+
+
+template <class T>
+std::vector<T> LinkedList<T>::toArray() const{
+    std::vector<T> final;
+    while (! theList.empty()) {
+        final.push_back(theList.front());
+        theList.pop_front();
+    }
+
+    return final;
+}
+
+
+template <class T>
+LinkedList<T>& LinkedList<T>::operator+=(const T& item) {
+    theList.append(item);
+    
+    return *theList;
+
+}
+
+
 
 #endif
